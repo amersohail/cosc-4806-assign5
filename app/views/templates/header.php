037f65542f5a201364df35fdb9caa9b82e94a75c
amersohail <amersohail7@gmail.com>
2024-07-08 20:56:36 +0000

    updated nav to show logged in user and added a log out button directly in the nav

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -17,29 +17,38 @@ if (!isset($_SESSION['auth'])) {
   
     <body>
       
-
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container">
-          <a class="navbar-brand" href="#">COSC 4806</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/home">Home</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Reminders
-                </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="/reminders/">View Reminders</a></li>
-                  <li><a class="dropdown-item" href="/reminders/create">Create Reminder</a></li>
-                  <li><a class="dropdown-item" href="/reminders/edit">Update/Delete Reminders</a></li>
-                </ul>
-              </li>
-            </ul>
+          <div class="container">
+              <a class="navbar-brand" href="#">COSC 4806</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                  <ul class="navbar-nav me-auto">
+                      <li class="nav-item">
+                          <a class="nav-link active" aria-current="page" href="/home">Home</a>
+                      </li>
+                      <li class="nav-item dropdown">
+                          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                              Reminders
+                          </a>
+                          <ul class="dropdown-menu">
+                              <li><a class="dropdown-item" href="/reminders/">View Reminders</a></li>
+                              <li><a class="dropdown-item" href="/reminders/create">Create Reminder</a></li>
+                              <li><a class="dropdown-item" href="/reminders/edit">Update/Delete Reminders</a></li>
+                          </ul>
+                      </li>
+                  </ul>
+                  <ul class="navbar-nav ms-auto">
+                      <li class="nav-item dropdown">
+                          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                              <?php echo $_SESSION['username']; ?>
+                          </a>
+                          <ul class="dropdown-menu dropdown-menu-end">
+                              <li><a class="dropdown-item" href="/logout">Logout</a></li>
+                          </ul>
+                      </li>
+                  </ul>
+              </div>
           </div>
-        </div>
       </nav>
